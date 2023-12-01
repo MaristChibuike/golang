cdb74662df9b8d1018ba0196fe0fdeb33c89fae5
MaristChibuike  git config --global user.name MaristChibuike <you@example.com>
2023-12-01 12:46:36 +0100

    New File added

---
+++ func.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//Write a function in Go that takes two integers as parameters and returns their sum.
+func sum(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+//Create a function that accepts a variable number of strings and concatenates them into a single string.
+func name(firstName string, lastName string, otherNames string) string {
+	fullName := fmt.Sprintf("%v %v %v", firstName, lastName, otherNames)
+	return fullName
+}
+func main() {
+	fmt.Println(sum(2, 3))
+	fmt.Println(name("Marist", "Chibuike", "O"))
+}
