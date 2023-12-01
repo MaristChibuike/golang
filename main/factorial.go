37f649137f60ed354183d1454cd2ab78be1d5837
MaristChibuike  git config --global user.name MaristChibuike <you@example.com>
2023-12-01 16:13:25 +0100

    New file factorial

---
+++ main/factorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func factorial(n int) int {
+	f := n // Holds the first value of n
+	for i := 1; i < n; i++ {
+		f *= (n - i)
+	}
+	return f
+}
+func main() {
+	fmt.Print(factorial(10))
+}
